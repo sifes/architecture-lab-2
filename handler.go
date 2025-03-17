dbdc5d903f023f1d5ec2f5fbfac6529c18253b08
sifes <131526240+sifes@users.noreply.github.com>
2025-03-17 04:19:42 -0700

    add handler_test.go

--- handler.go
+++ handler.go
@@ -1,30 +1,27 @@
 package lab2
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"strings"
+  "fmt"
+  "io"
+  "io/ioutil"
+  "strings"
 )
 
 type ComputeHandler struct {
-	Reader io.Reader
-	Writer io.Writer
+  Reader io.Reader
+  Writer io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	data, err := ioutil.ReadAll(ch.Reader)
-	if err != nil {
-		return fmt.Errorf("error reading input: %w", err)
-	}
-
-	expression := strings.TrimSpace(string(data))
-
-	result, err := EvaluatePrefix(expression)
-	if err != nil {
-		return err
-	}
-
-	_, err = fmt.Fprintln(ch.Writer, result)
-	return err
+  data, err := ioutil.ReadAll(ch.Reader)
+  if err != nil {
+    return fmt.Errorf("error reading input: %w", err)
+  }
+  expression := strings.TrimSpace(string(data))
+  result, err := EvaluatePrefix(expression)
+  if err != nil {
+    return err
+  }
+  _, err = fmt.Fprintln(ch.Writer, result)
+  return err
 }
--- handler_test.go
+++ handler_test.go
@@ -1,21 +1,89 @@
 package lab2
 
 import (
-	. "gopkg.in/check.v1"
+	"bytes"
+	"strings"
+	"testing"
+	
+	"github.com/stretchr/testify/assert"
 )
 
-type PrefixComputeSuite struct{}
-
-var _ = Suite(&PrefixComputeSuite{})
-
-func (s *PrefixComputeSuite) TestCalculatePrefix(c *C) {
-	res, err := EvaluatePrefix("* + 2 3 5")
-	c.Assert(err, IsNil)
-	c.Assert(res, Equals, "25")
+func TestComputeHandlerValidInput(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected string
+	}{
+		{"+ 2 3", "5\n"},
+		{"* + 2 3 5", "25\n"},
+		{"/ 10 2", "5\n"},
+		{"+ + 4 / - * 6 2 3 2 5", "13.50\n"},
+	}
+	
+	for _, tc := range testCases {
+		var outputBuffer bytes.Buffer
+		
+		handler := &ComputeHandler{
+			Reader: strings.NewReader(tc.input),
+			Writer: &outputBuffer,
+		}
+		
+		err := handler.Compute()
+		
+		assert.NoError(t, err)
+		
+		assert.Equal(t, tc.expected, outputBuffer.String())
+	}
 }
 
-func (s *PrefixComputeSuite) TestCalculatePrefixError(c *C) {
-	res, err := EvaluatePrefix("+ 2 invalid")
-	c.Assert(err, NotNil)
-	c.Assert(res, Equals, "")
+func TestComputeHandlerInvalidInput(t *testing.T) {
+	testCases := []struct {
+		input       string
+		errorSubstr string
+	}{
+		{"", "empty input"},
+		{"+ 2", "not enough operands"},
+		{"$ 2 3", "unsupported symbol"},
+		{"+ 2 invalid", "unsupported symbol"},
+		{"/ 5 0", "division by zero"},
+	}
+	
+	for _, tc := range testCases {
+		var outputBuffer bytes.Buffer
+		
+		handler := &ComputeHandler{
+			Reader: strings.NewReader(tc.input),
+			Writer: &outputBuffer,
+		}
+		
+		err := handler.Compute()
+		
+		assert.Error(t, err)
+		
+		assert.Contains(t, err.Error(), tc.errorSubstr)
+		
+		assert.Empty(t, outputBuffer.String())
+	}
+}
+
+type ErrorReader struct{}
+
+func (r *ErrorReader) Read(p []byte) (n int, err error) {
+	return 0, assert.AnError
+}
+
+func TestComputeHandlerReaderError(t *testing.T) {
+	var outputBuffer bytes.Buffer
+	
+	handler := &ComputeHandler{
+		Reader: &ErrorReader{},
+		Writer: &outputBuffer,
+	}
+	
+	err := handler.Compute()
+	
+	assert.Error(t, err)
+	
+	assert.Contains(t, err.Error(), "error reading input")
+	
+	assert.Empty(t, outputBuffer.String())
 }
